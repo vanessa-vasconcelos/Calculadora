b506afa95a386d86b55fb8f9757d146f2333f234
Vanessa <104469671+vanessa-vasconcelos@users.noreply.github.com>
2022-07-27 15:56:05 -0300

    Criação da calculadora

---
+++ script.js
@@ -0,0 +1,76 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+
+        inicia() {
+            this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        apagarUm() {
+            // Apaga somente um número
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if (!conta) {
+                    alert('Conta inválida!');
+                    return;
+                }
+
+                this.display.value = String(conta);
+            } catch (e) {
+                alert('Conta inválida!');
+                return;
+            }
+        },
+
+
+        // this -> calculadora
+        cliqueBotoes() {
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                // se o elemento (el) contem essa classe (btn-num)
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if (el.classList.contains('btn-del')) {
+                    this.apagarUm();
+                }
+
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+            });
+        },
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        },
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
