80e831e454b19fa9c3851a31aa215508252f25fa
Vanessa <vanessa.vasconcelos434@gmail.com>
2024-08-24 22:04:24 -0300

    Finalização da Calculadora

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+    info = Info('Sua conta')
--- ulteis.py
+++ ulteis.py
@@ -5,6 +5,13 @@ NUM_OR_DOT_REGEX = re.compile(r'^[0-9.]$')
 def isNumOrDot(string: str):
   return bool(NUM_OR_DOT_REGEX.search(string))
 
+def converToNumber(string: str):
+  number = float(string)
+    
+  if number.is_integer():
+    number = int(number)
+  return number
+
 def isValidNumber(string: str):
   valid = False
   try:
